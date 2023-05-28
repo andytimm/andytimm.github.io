71f7390c40fca7f01e18e671edb498aba9236f54
Andrew Timm <timmandrew1@gmail.com>
2023-05-27 21:10:16 -0400

    intro + outline on Flows

---
+++ posts/Variational MRP Pt5/variational_mrp_pt5.R
@@ -0,0 +1,12 @@
+---
+  layout: post
+title: Variational Inference for MRP with Reliable Posterior Distributions
+subtitle: Part 5- Normalizing Flows
+date: 2023-05-27
+draft: True
+categories:
+  - MRP
+  - Variational Inference
+  - Normalizing Flows
+---
+  
